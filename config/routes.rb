07948671419d66a3fa8cd72fd80c77245798b8aa
EC2 Default User <ec2-user@ip-172-31-22-113.ap-northeast-1.compute.internal>
2023-06-26 12:04:02 +0000

    サインアップ

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
   root to: 'homes#top'
   get 'home/about', to: 'homes#about'
+  resources :users, only: [:index, :show, :edit, :update]
   resources :games, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 end
